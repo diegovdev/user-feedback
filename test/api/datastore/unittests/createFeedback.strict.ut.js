1b03df169b27f1fbfc2c67a66b036de675ca9e8a
Diego Carvallo <sapeish@gmail.com>
2018-11-11 19:38:11 -0400

    [+] adding unit tests

---
+++ test/api/datastore/unittests/createFeedback.strict.ut.js
@@ -0,0 +1,231 @@
+process.env.NODE_ENV = 'test';
+
+const sinon     = require('sinon');
+const chai      = require('chai');
+const chaiHttp  = require('chai-http');
+const sinonChai = require("sinon-chai");
+chai.use(chaiHttp);
+chai.use(sinonChai);
+const server    = require('../../../../app');
+const config    = require('../../../../config/config');
+const datastore = require('../../../../lib/datastore');
+const expect    = chai.expect;
+const request   = chai.request;
+
+//to mock
+const memoryDS  = require('../../../../lib/datastore/memoryDS');
+
+let sandbox = null;
+
+describe('createFeedback.strict.ut', () => {
+    before(function (done) {
+        config.set('app:datastore', 'memory');
+        config.set('app:validationMode', 'strict');
+        datastore.loadConfigs();
+        done();
+    });
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create()
+    });
+    afterEach(function () {
+        sandbox.restore()
+    });
+
+    it('should fail to create a new Feedback when userId doesnt exist', (done) => {
+        //--GIVEN-------------------------------------------------------------------------------------------------------
+        let userId = "ut-user-1001";
+        let sessionId = "ut-sesssion-2001";
+        let newFeedback = {
+            "Ubi-UserId": userId,
+            "rating": "2",
+            "comment": "bad connection"
+        };
+        let existsUser = sandbox.stub(memoryDS, 'existsUser').callsFake(async function (user) { return false; });
+        let existsSession = sandbox.stub(memoryDS, 'existsSession').callsFake(async function (session) { return true; });
+
+        //--WHEN--------------------------------------------------------------------------------------------------------
+        request(server)
+        .post('/api/v1/feedback/create/' + sessionId)
+        .send(newFeedback)
+        .end((err, res) => {
+            //--THEN----------------------------------------------------------------------------------------------------
+            expect(existsUser).to.have.been.calledWith();
+            expect(existsSession).to.have.been.calledWith();
+            expect(res.status).to.equal(200);
+            expect(res.body).to.not.equal(null);
+            expect(res.body).to.be.a('object');
+            expect(res.body.status).to.equal('error');
+            expect(res.body.error).to.not.equal(null);
+            expect(res.body.error).to.be.a('object');
+            expect(res.body.error.message).to.equal("User with id="+userId+" does not exist.");
+            done();
+        });
+    });
+
+    it('should fail to create a new Feedback when sessionId doesnt exist', (done) => {
+        //--GIVEN-------------------------------------------------------------------------------------------------------
+        let userId = "ut-user-1001";
+        let sessionId = "ut-sesssion-2001";
+        let newFeedback = {
+            "Ubi-UserId": userId,
+            "rating": "2",
+            "comment": "bad connection"
+        };
+        let existsUser = sandbox.stub(memoryDS, 'existsUser').callsFake(async function (user) { return true; });
+        let existsSession = sandbox.stub(memoryDS, 'existsSession').callsFake(async function (session) { return false; });
+
+        //--WHEN--------------------------------------------------------------------------------------------------------
+        request(server)
+        .post('/api/v1/feedback/create/' + sessionId)
+        .send(newFeedback)
+        .end((err, res) => {
+            //--THEN----------------------------------------------------------------------------------------------------
+            expect(existsUser).to.have.been.calledWith();
+            expect(existsSession).to.have.been.calledWith();
+            expect(res.status).to.equal(200);
+            expect(res.body).to.not.equal(null);
+            expect(res.body).to.be.a('object');
+            expect(res.body.status).to.equal('error');
+            expect(res.body.error).to.not.equal(null);
+            expect(res.body.error).to.be.a('object');
+            expect(res.body.error.message).to.equal("Session with id="+sessionId+" does not exist.");
+            done();
+        });
+    });
+
+    it('should fail to create a new Feedback when entry already exists for provided sessionId and userId', (done) => {
+        //--GIVEN-------------------------------------------------------------------------------------------------------
+        let userId = "ut-user-1001";
+        let sessionId = "ut-sesssion-2001";
+        let newFeedback = {
+            "Ubi-UserId": userId,
+            "rating": "2",
+            "comment": "bad connection"
+        };
+        let existsUser = sandbox.stub(memoryDS, 'existsUser').callsFake(async function (user) { return true; });
+        let existsSession = sandbox.stub(memoryDS, 'existsSession').callsFake(async function (session) { return true; });
+        let existsFeedback = sandbox.stub(memoryDS, 'existsFeedback').callsFake(async function (feedback) { return true; });
+
+        //--WHEN--------------------------------------------------------------------------------------------------------
+        request(server)
+        .post('/api/v1/feedback/create/' + sessionId)
+        .send(newFeedback)
+        .end((err, res) => {
+            //--THEN----------------------------------------------------------------------------------------------------
+            expect(existsUser).to.have.been.calledWith();
+            expect(existsSession).to.have.been.calledWith();
+            expect(existsFeedback).to.have.been.calledWith();
+            expect(res.status).to.equal(200);
+            expect(res.body).to.not.equal(null);
+            expect(res.body).to.be.a('object');
+            expect(res.body.status).to.equal('error');
+            expect(res.body.error).to.not.equal(null);
+            expect(res.body.error).to.be.a('object');
+            expect(res.body.error.message).to.equal("Feedback for sessionId="+sessionId+" and userId="+userId+" was already submitted.");
+            done();
+        });
+    });
+
+    it('should create a new Feedback when sessionId and userId exist', (done) => {
+        //--GIVEN-------------------------------------------------------------------------------------------------------
+        let userId = "ut-user-1001";
+        let sessionId = "ut-sesssion-2001";
+        let feedbackId = 3001;
+        let newFeedback = {
+            "Ubi-UserId": userId,
+            "rating": "2",
+            "comment": "bad connection"
+        };
+        let existsUser = sandbox.stub(memoryDS, 'existsUser').callsFake(async function (user) { return true; });
+        let existsSession = sandbox.stub(memoryDS, 'existsSession').callsFake(async function (session) { return true; });
+        let existsFeedback = sandbox.stub(memoryDS, 'existsFeedback').callsFake(async function (feedback) { return false; });
+        let createFeedback = sandbox.stub(memoryDS, 'createFeedback').callsFake(async function (feedback) { return feedbackId; });
+
+        //--WHEN--------------------------------------------------------------------------------------------------------
+        request(server)
+        .post('/api/v1/feedback/create/' + sessionId)
+        .send(newFeedback)
+        .end((err, res) => {
+            //--THEN----------------------------------------------------------------------------------------------------
+            expect(existsUser).to.have.been.calledWith();
+            expect(existsSession).to.have.been.calledWith();
+            expect(existsFeedback).to.have.been.calledWith();
+            expect(createFeedback).to.have.been.calledWith();
+            expect(res.status).to.equal(200);
+            expect(res.body).to.not.equal(null);
+            expect(res.body).to.be.a('object');
+            expect(res.body.status).to.equal('ok');
+            expect(res.body.data.id).to.equal(feedbackId);
+            done();
+        });
+    });
+
+    it('should create a new Feedback taking userId from body', (done) => {
+        //--GIVEN-------------------------------------------------------------------------------------------------------
+        let userId = "ut-user-1001";
+        let sessionId = "ut-sesssion-2001";
+        let feedbackId = 3001;
+        let newFeedback = {
+            "Ubi-UserId": userId,
+            "rating": "2",
+            "comment": "bad connection"
+        };
+        let existsUser = sandbox.stub(memoryDS, 'existsUser').callsFake(async function (user) { return true; });
+        let existsSession = sandbox.stub(memoryDS, 'existsSession').callsFake(async function (session) { return true; });
+        let existsFeedback = sandbox.stub(memoryDS, 'existsFeedback').callsFake(async function (feedback) { return false; });
+        let createFeedback = sandbox.stub(memoryDS, 'createFeedback').callsFake(async function (feedback) { return feedbackId; });
+
+        //--WHEN--------------------------------------------------------------------------------------------------------
+        request(server)
+        .post('/api/v1/feedback/create/' + sessionId)
+        .send(newFeedback)
+        .end((err, res) => {
+            //--THEN----------------------------------------------------------------------------------------------------
+            expect(existsUser).to.have.been.calledWith();
+            expect(existsSession).to.have.been.calledWith();
+            expect(existsFeedback).to.have.been.calledWith();
+            expect(createFeedback).to.have.been.calledWith();
+            expect(res.status).to.equal(200);
+            expect(res.body).to.not.equal(null);
+            expect(res.body).to.be.a('object');
+            expect(res.body.status).to.equal('ok');
+            expect(res.body.data.id).to.equal(feedbackId);
+            done();
+        });
+    });
+
+    it('should create a new Feedback taking userId from header', (done) => {
+        //--GIVEN-------------------------------------------------------------------------------------------------------
+        let userId = "ut-user-1001";
+        let sessionId = "ut-sesssion-2001";
+        let feedbackId = 3001;
+        let newFeedback = {
+            "rating": "2",
+            "comment": "bad connection"
+        };
+        let existsUser = sandbox.stub(memoryDS, 'existsUser').callsFake(async function (user) { return true; });
+        let existsSession = sandbox.stub(memoryDS, 'existsSession').callsFake(async function (session) { return true; });
+        let existsFeedback = sandbox.stub(memoryDS, 'existsFeedback').callsFake(async function (feedback) { return false; });
+        let createFeedback = sandbox.stub(memoryDS, 'createFeedback').callsFake(async function (feedback) { return feedbackId; });
+
+        //--WHEN--------------------------------------------------------------------------------------------------------
+        request(server)
+        .post('/api/v1/feedback/create/' + sessionId)
+        .set('Ubi-UserId', userId)
+        .send(newFeedback)
+        .end((err, res) => {
+            //--THEN----------------------------------------------------------------------------------------------------
+            expect(existsUser).to.have.been.calledWith();
+            expect(existsSession).to.have.been.calledWith();
+            expect(existsFeedback).to.have.been.calledWith();
+            expect(createFeedback).to.have.been.calledWith();
+            expect(res.status).to.equal(200);
+            expect(res.body).to.not.equal(null);
+            expect(res.body).to.be.a('object');
+            expect(res.body.status).to.equal('ok');
+            expect(res.body.data.id).to.equal(feedbackId);
+            done();
+        });
+    });
+
+});
---
+++ test/api/datastore/unittests/retrieveFeedback.ut.js
@@ -0,0 +1,66 @@
+process.env.NODE_ENV = 'test';
+
+const sinon     = require('sinon');
+const chai      = require('chai');
+const chaiHttp  = require('chai-http');
+const sinonChai = require("sinon-chai");
+chai.use(chaiHttp);
+chai.use(sinonChai);
+const server    = require('../../../../app');
+const config    = require('../../../../config/config');
+const datastore = require('../../../../lib/datastore');
+const expect    = chai.expect;
+const request   = chai.request;
+
+//to mock
+const memoryDS  = require('../../../../lib/datastore/memoryDS');
+
+let sandbox = null;
+
+describe('retrieveFeedback.ut', () => {
+    before(function (done) {
+        config.set('app:datastore', 'memory');
+        config.set('app:validationMode', 'permissive');
+        datastore.loadConfigs();
+        done();
+    });
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create()
+    });
+    afterEach(function () {
+        sandbox.restore()
+    });
+
+    it('should GET a single Feedback', (done) => {
+        //--GIVEN-------------------------------------------------------------------------------------------------------
+        let feedbackId = 3;
+        let existingFeedback = {
+            "id": feedbackId,
+            "rating": "4",
+            "comment": "all good",
+            "createdAt": "2018-11-11T15:08:16.000Z",
+            "updatedAt": "2018-11-11T15:08:16.000Z",
+            "userId": "user-from-header-002",
+            "sessionId": "ses-555-0001"
+        };
+        let getFeedback = sandbox.stub(memoryDS, 'getFeedback').callsFake(async function (feedbackId) { return [ existingFeedback ]; });
+
+        //--WHEN--------------------------------------------------------------------------------------------------------
+        request(server)
+        .get('/api/v1/feedback/find/' + feedbackId)
+        .end((err, res) => {
+            //--THEN----------------------------------------------------------------------------------------------------
+            expect(getFeedback).to.have.been.calledWith();
+            expect(res.status).to.equal(200);
+            expect(res.body).to.not.equal(null);
+            expect(res.body).to.be.a('object');
+            expect(res.body.status).to.equal('ok');
+            expect(res.body.data.count).to.equal(1);
+            expect(res.body.data.result).to.be.a('array');
+            expect(res.body.data.result[0]).to.be.a('object');
+            expect(res.body.data.result[0].id).to.equal(feedbackId);
+            done();
+        });
+    });
+
+});
